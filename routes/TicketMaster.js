e9b2c63dec2c062c646db49828629c480d356fd1
DharviPatel2805 <dharvi.pict19@sot.pdpu.ac.in>
2024-09-11 13:01:23 +0530

    routes tickets master

---
+++ routes/TicketMaster.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const router = express.Router();
+
+const catchAsync = require("../utils/catchAsync");
+const { createTicketMaster, listTicketMasterByParams, listTicketMaster, getTicketMaster, updateTicketMaster, removeTicketMaster } = require("../controllers/Master/TicketMaster");
+
+router.post("/auth/ticketMaster", catchAsync(createTicketMaster));
+router.get("/auth/list/ticketMaster", catchAsync(listTicketMaster));
+
+router.post(
+    "/auth/list-by-params/ticketMaster",
+    catchAsync(listTicketMasterByParams)
+);
+
+router.get("/auth/get/ticketMaster/:_id", catchAsync(getTicketMaster));
+
+router.put(
+    "/auth/update/ticketMaster/:_id",
+    catchAsync(updateTicketMaster)
+);
+
+router.delete(
+    "/auth/remove/ticketMaster/:_id",
+    catchAsync(removeTicketMaster)
+);
+
+
+module.exports = router;
