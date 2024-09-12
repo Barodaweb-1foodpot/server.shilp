4629c2e47414ec668137c5c09537574beb0148d2
Krish25V <krishviramgama25@gmail.com>
2024-09-12 12:12:32 +0530

    zzz

--- routes/StageOfStartup.js
+++ routes/StageOfStartup.js
@@ -14,22 +14,22 @@ const {
 } = require("../controllers/Master/StageOfStartup");
 
 
-router.post("/auth/create/categoryMaster", catchAsync(createStageOfStartup));
+router.post("/auth/create/StageOfStartup", catchAsync(createStageOfStartup));
 
-router.get("/auth/list/categoryMaster", catchAsync(listStageOfStartup));
+router.get("/auth/list/StageOfStartup", catchAsync(listStageOfStartup));
 
-router.get("/auth/get/categoryMaster/:_id", catchAsync(getStageOfStartup));
+router.get("/auth/get/StageOfStartup/:_id", catchAsync(getStageOfStartup));
 
 router.put(
-  "/auth/update/categoryMaster/:_id",
+  "/auth/update/StageOfStartup/:_id",
   catchAsync(updateStageOfStartup)
 );
 
 router.delete(
-  "/auth/remove/categoryMaster/:_id",
+  "/auth/remove/StageOfStartup/:_id",
   catchAsync(removeStageOfStartup)
 );
 
-router.get("/auth/get/list/ActiveCategories" , catchAsync(listActiveStageOfStartup));
+router.get("/auth/get/list/StageOfStartup" , catchAsync(listActiveStageOfStartup));
 
 module.exports = router;
